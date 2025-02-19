963680dd8898406c51775132300aae83745518f7
Stevenozzo <stevencarlino46@live.com>
2025-02-19 16:20:04 +0100

    bugfixing

--- finance-app/src/app/app.routes.ts
+++ finance-app/src/app/app.routes.ts
@@ -1,3 +1,9 @@
-import { Routes } from '@angular/router';
+import { Routes } from '@angular/router'; // Import the Home Component
+import { HomeComponent } from './home/home.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: '', // This makes HomeComponent the default route
+    component: HomeComponent,
+  },
+];
--- finance-app/src/app/routes.ts
+++
@@ -1,6 +0,0 @@
-import { Routes } from '@angular/router';
-import { HomeComponent } from './home/home.component';
-
-export const routeConfig: Routes = [
-  { path: '', component: HomeComponent, title: 'Home Page' },
-];
